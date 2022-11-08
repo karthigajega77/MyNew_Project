bd57b06baf4296c280806c31ea7cb117904c731a
karthiga <karthiga77@gmail.com>
2022-11-08 07:47:05 +0530

    My Project

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+import org.junit.Test;
+
+public class Sample {
+@Test
+public void test() {
+System.out.println("Test 1");
+System.out.println("Test 2");
+System.out.println("Test 3");
+}
+
+}
